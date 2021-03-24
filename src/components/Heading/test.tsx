3f0b94e0060ba109e13bfce3a44d6a1c34fc2772
Rafael Souza <devrafaelsouza@gmail.com>
2021-03-23 22:34:23 -0300

    Criando component heading e suas variações.

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
+
+import Heading from '.';
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>);
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    });
+  });
+});
