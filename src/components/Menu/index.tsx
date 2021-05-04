d2176b1a8468c86fe1491d42a183ced7761a1f81
Rafael Souza <devrafaelsouza@gmail.com>
2021-05-03 21:51:17 -0300

    Criando lógica de abrir/fechar e TDD do menu.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,39 @@
+import { useState } from 'react';
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import Logo from 'components/Logo';
 
 import * as S from './styles';
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideTextOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
-      </S.IconWrapper>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open shopping cart" />
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-);
+      <S.LogoWrapper>
+        <Logo hideTextOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
 
 export default Menu;
