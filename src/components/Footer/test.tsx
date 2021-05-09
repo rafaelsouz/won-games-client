94e331c444404c71c18afedf14229d5f2a30bae5
Rafael Souza <devrafaelsouza@gmail.com>
2021-05-09 18:56:42 -0300

    Criando o Footer e testes como exercício.

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
+
+import Footer from '.';
+
+describe('<Footer />', () => {
+  it('deve renderizar a 4 colunas de informações', () => {
+    const { container } = renderWithTheme(<Footer />);
+
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
