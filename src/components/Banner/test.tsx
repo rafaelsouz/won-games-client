18439fb34328a30ffdf00d16bc01377f1991ffa9
Rafael Souza <devrafaelsouza@gmail.com>
2021-05-16 18:12:00 -0300

    Criando estrutura inicial do banner.

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import Banner from '.';
+
+describe('<Banner />', () => {
+  it('deve renderizar o banner corretamente', () => {
+    // verifique se o title existe renderizado (.toBeInTheDocument())
+    // verifique se o subtitle existe renderizado
+    // verifique se a imagem existe renderizado
+  });
+});
