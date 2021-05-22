27c8a717dd3a655f5611d827327aef37edbd4b64
Rafael Souza <devrafaelsouza@gmail.com>
2021-05-22 20:00:07 -0300

    Escrevendo os testes.

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,9 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
 
 import Banner from '.';
 
 describe('<Banner />', () => {
   it('deve renderizar o banner corretamente', () => {
+    renderWithTheme(
+      <Banner
+        title="Promoção de verão"
+        subtitle="Desconto de 50% em todos os jogos."
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        buttonLabel="Compre agora"
+        buttonLink="/games/defy-death"
+      />
+    );
+
+    expect(
+      screen.getByRole('heading', { name: /Promoção de verão/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Desconto de 50% em todos os jogos./i
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('img', {
+        name: /Promoção de verão/i
+      })
+    ).toBeInTheDocument();
+
     // verifique se o title existe renderizado (.toBeInTheDocument())
     // verifique se o subtitle existe renderizado
     // verifique se a imagem existe renderizado
