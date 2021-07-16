d1282680ef592dab153395d00d4635a73f8b8673
Rafael <devrafaelsouza@gmail.com>
2021-07-16 15:37:01 -0300

    Criando teste e mock para matchMedia.

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,13 +1,26 @@
+import '../../../.jest/match-media-mock';
+
 import { render, screen } from '@testing-library/react';
 
 import Slider from '.';
 
 describe('<Slider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Slider />);
+  it('deve renderizar o children como sliderItem', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    );
 
     expect(
-      screen.getByRole('heading', { name: /Slider/i })
-    ).toBeInTheDocument();
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
