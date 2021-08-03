de2e795325b5169488cf57fbb5e92ef2137cebb2
Rafael <devrafaelsouza@gmail.com>
2021-08-03 16:34:17 -0300

    Adicionando os primeiros componentes estáticos e criando testes.

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,44 @@
+import { Container } from 'components/Container';
+import Footer from 'components/Footer';
+import Menu from 'components/Menu';
+import Heading from 'components/Heading';
+
 import * as S from './styles';
 
 const Home = () => (
-  <S.Wrapper>
-    <h1>Home</h1>
-  </S.Wrapper>
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Upcoming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
 );
 
 export default Home;
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,13 +1,34 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
 
-import Home from '.'
+import Home from '.';
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Home />)
+  it('deve renderizar o menu e o footer', () => {
+    renderWithTheme(<Home />);
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot()
-  })
-})
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument();
+  });
+
+  it('deve renderizar as secções', () => {
+    renderWithTheme(<Home />);
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Upcoming/i })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i })
+    ).toBeInTheDocument();
+  });
+});
