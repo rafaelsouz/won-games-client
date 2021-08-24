f29ec466015f34b12099afc8da519df71ea8368d
Rafael <devrafaelsouza@gmail.com>
2021-08-23 23:12:13 -0300

    Criando os mocks de dados.

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,36 +3,78 @@ import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import Heading from 'components/Heading';
 
-import * as S from './styles';
+import { BannerProps } from 'components/Banner';
+import { GameCardProps } from 'components/GameCard';
+import Highlight, { HighlightProps } from 'components/Highlight';
 
-const Home = () => (
+import * as S from './styles';
+import BannerSlider from 'components/BannerSlider';
+import GameCardSlider from 'components/GameCardSlider';
+
+export type HomeTemplateProps = {
+  banners: BannerProps[];
+  newGames: GameCardProps[];
+  mostPopularHighlight: HighlightProps;
+  mostPopularGames: GameCardProps[];
+  upcomingGames: GameCardProps[];
+  upcomingHighlight: HighlightProps;
+  upcomingMoreGames: GameCardProps[];
+  freeGames: GameCardProps[];
+  freeHighlight: HighlightProps;
+};
+
+const Home = ({
+  banners,
+  freeGames,
+  freeHighlight,
+  mostPopularGames,
+  mostPopularHighlight,
+  newGames,
+  upcomingGames,
+  upcomingHighlight,
+  upcomingMoreGames
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="black">
         News
       </Heading>
+
+      <GameCardSlider items={newGames} color="black" />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="white">
         Most Popular
       </Heading>
+
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={mostPopularGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="white">
         Upcoming
       </Heading>
+
+      <GameCardSlider items={upcomingGames} />
+      <Highlight {...upcomingHighlight} />
+      <GameCardSlider items={upcomingMoreGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="white">
         Free Games
       </Heading>
+
+      <Highlight {...freeHighlight} />
+      <GameCardSlider items={freeGames} />
     </Container>
 
     <Container>
