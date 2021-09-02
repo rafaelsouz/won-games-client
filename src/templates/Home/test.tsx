603f6dd8c1b50497efcdea84b96cd5b8220b3aa3
Rafael <devrafaelsouza@gmail.com>
2021-09-02 14:20:01 -0300

    Otimizando os testes

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,34 +1,56 @@
+import '../../../.jest/match-media-mock';
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helper';
 
+import bannerMock from 'components/BannerSlider/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightMock from 'components/Highlight/mock';
+
 import Home from '.';
 
-describe('<Home />', () => {
-  it('deve renderizar o menu e o footer', () => {
-    renderWithTheme(<Home />);
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighlight: highlightMock
+};
 
+describe('<Home />', () => {
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home {...props} />);
+
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
 
+    // footer
     expect(
-      screen.getByRole('heading', { name: /contact/i })
-    ).toBeInTheDocument();
-  });
-
-  it('deve renderizar as secções', () => {
-    renderWithTheme(<Home />);
-
-    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument();
-
-    expect(
-      screen.getByRole('heading', { name: /Most Popular/i })
+      screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument();
 
+    // logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /Upcoming/i })
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /upcoming/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument();
 
-    expect(
-      screen.getByRole('heading', { name: /Free Games/i })
-    ).toBeInTheDocument();
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1);
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5);
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3);
   });
 });
