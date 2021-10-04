95bdaab5ae24adb0293d0de118f6f85892f7305d
Rafael <devrafaelsouza@gmail.com>
2021-10-04 14:46:06 -0300

    Criando estilos do FormSignIn

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,13 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
 
 import FormSignIn from '.';
 
 describe('<FormSignIn />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignIn />);
+  it('Deve renderiza o formulario', () => {
+    const { container } = renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Sign in now/i })
+    ).toBeInTheDocument();
+
+    expect(container.parentElement).toMatchSnapshot();
+  });
+
+  it('Deve renderiza o link de esqueci minha senha', () => {
+    renderWithTheme(<FormSignIn />);
 
     expect(
-      screen.getByRole('heading', { name: /FormSignIn/i })
+      screen.getByRole('link', { name: /Forgot your password?/i })
     ).toBeInTheDocument();
+  });
+
+  it('Deve renderiza o texto para cria conta', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByRole('link', { name: /Sign up/i })).toBeInTheDocument();
+    expect(screen.getByText('Don´t have an account?')).toBeInTheDocument();
   });
 });
