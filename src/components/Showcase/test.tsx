01a319131f3913154d9389cfa804cd64340f7b14
Rafael <devrafaelsouza@gmail.com>
2021-10-13 13:52:43 -0300

    feat: Criando testes para o showcase

--- src/components/Showcase/test.tsx
+++ src/components/Showcase/test.tsx
@@ -1,13 +1,69 @@
-import { render, screen } from '@testing-library/react'
+import '../../../.jest/match-media-mock';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
 
-import ShowCase from '.'
+import highlightMock from 'components/Highlight/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+
+import Showcase from '.';
+
+const props = {
+  title: 'Most Popular',
+  highlight: highlightMock,
+  games: gamesMock.slice(0, 1)
+};
 
 describe('<ShowCase />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<ShowCase />)
+  it('deve renderizar showcase completo', () => {
+    renderWithTheme(<Showcase {...props} />);
 
-    expect(screen.getByRole('heading', { name: /ShowCase/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot()
-  })
-})
+    expect(
+      screen.getByRole('heading', { name: highlightMock.title })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: gamesMock[0].title })
+    ).toBeInTheDocument();
+  });
+
+  it('deve renderizar sem titulo', () => {
+    renderWithTheme(
+      <Showcase games={props.games} highlight={props.highlight} />
+    );
+
+    screen.getByRole('heading', { name: highlightMock.title });
+    screen.getByRole('heading', { name: gamesMock[0].title });
+
+    expect(
+      screen.queryByRole('heading', { name: /most popular/i })
+    ).not.toBeInTheDocument();
+  });
+
+  it('deve renderizar sem highlight', () => {
+    renderWithTheme(<Showcase title={props.title} games={props.games} />);
+
+    screen.getByRole('heading', { name: /most popular/i });
+    screen.getByRole('heading', { name: gamesMock[0].title });
+
+    expect(
+      screen.queryByRole('heading', { name: highlightMock.title })
+    ).not.toBeInTheDocument();
+  });
+
+  it('deve renderizar sem jogos', () => {
+    renderWithTheme(
+      <Showcase title={props.title} highlight={props.highlight} />
+    );
+
+    screen.getByRole('heading', { name: /most popular/i });
+    screen.getByRole('heading', { name: highlightMock.title });
+
+    expect(
+      screen.queryByRole('heading', { name: gamesMock[0].title })
+    ).not.toBeInTheDocument();
+  });
+});
