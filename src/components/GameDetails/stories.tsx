58ca9658ce1a415c4864d1464516f50b2a954bd7
Rafael <devrafaelsouza@gmail.com>
2021-11-03 16:45:03 -0300

    feat: Trabalhando com argTypes do Storybook para types diferentes.

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import GameDetails from '.';
+import GameDetails, { GameDetailsProps } from '.';
 
 export default {
   title: 'Game/GameDetails',
@@ -8,11 +8,22 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    platforms: ['Windows', 'Linux', 'Mac']
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['Windows', 'Linux', 'Mac']
+      }
+    }
   }
 } as Meta;
 
-export const Default: Story = () => (
+export const Default: Story<GameDetailsProps> = (args) => (
   <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-    <GameDetails platforms={['Windows', 'Linux', 'Mac']} />
+    <GameDetails {...args} />
   </div>
 );
