e61ada979ac1198b8f4262b6f277e9aaadb04eb5
Rafael <devrafaelsouza@gmail.com>
2021-11-12 16:22:22 -0300

    feat: Criando testes.

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -1,13 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helper';
 
 import TextContent from '.';
 
+const textProps = {
+  title: 'Description',
+  content: `<h1>Content</h1>
+  `
+};
+
 describe('<TextContent />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<TextContent />);
+  it('deve renderizar o título e conteúdo', () => {
+    renderWithTheme(<TextContent {...textProps} />);
 
     expect(
-      screen.getByRole('heading', { name: /TextContent/i })
+      screen.getByRole('heading', { name: /description/i })
     ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /content/i })
+    ).toBeInTheDocument();
+  });
+
+  it('deve renderizar sem o título', () => {
+    renderWithTheme(<TextContent content={textProps.content} />);
+
+    expect(
+      screen.queryByRole('heading', { name: /description/i })
+    ).not.toBeInTheDocument();
+  });
+
+  it('deve renderizar o título e conteúdo', () => {
+    renderWithTheme(<TextContent {...textProps} />);
+
+    const wrapper = screen.getByRole('heading', { name: /description/i })
+      .parentElement;
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    });
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    });
   });
 });
