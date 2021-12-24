38a652a11b633e0c778b348bd462b614f9bcc546
Rafael <devrafaelsouza@gmail.com>
2021-12-24 12:15:16 -0300

    feat: Criando a cover image

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -4,7 +4,13 @@ import * as S from './styles';
 
 const Game = () => (
   <Base>
-    <h1>Game</h1>
+    <S.Cover
+      src={
+        'https://p2.trrsf.com/image/fget/cf/1200/628/middle/images.terra.com/2021/07/20/cyberpunk2077-05.jpg'
+      }
+      role="image"
+      aria-label="cover"
+    />
   </Base>
 );
 
