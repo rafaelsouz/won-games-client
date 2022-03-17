4da6eee6ffbffbdf26fb7126d3268b643c24176f
Rafael <devrafaelsouza@gmail.com>
2022-03-17 12:07:00 -0300

    feat: Escrevendo primeiros testes para o template.

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -3,14 +3,31 @@ import { Container } from 'components/Container';
 import Heading from 'components/Heading';
 
 import * as S from './styles';
+import { GameCardProps } from 'components/GameCard';
+import { HighlightProps } from 'components/Highlight';
+import Showcase from 'components/Showcase';
 
-const Wishlist = () => (
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[];
+  recommendedHighlight: HighlightProps;
+};
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 );
 
--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -2,12 +2,30 @@ import { render, screen } from '@testing-library/react';
 
 import Wishlist from '.';
 
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightMock from 'components/Highlight/mock';
+import { renderWithTheme } from 'utils/tests/helper';
+
+const props = {
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+};
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />;
+  }
+}));
+
 describe('<Wishlist />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Wishlist />);
+    renderWithTheme(<Wishlist {...props} />);
 
     expect(
       screen.getByRole('heading', { name: /Wishlist/i })
     ).toBeInTheDocument();
+
+    expect(screen.getByTestId(/mock showcase/i)).toBeInTheDocument();
   });
 });
