6f2868ba4c4d458b9883a8188625b48b130efde8
Rafael <devrafaelsouza@gmail.com>
2022-03-17 16:55:10 -0300

    feat: Criando o componente de Grid

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,11 +1,13 @@
 import Base from 'templates/Base';
+
 import { Container } from 'components/Container';
 import Heading from 'components/Heading';
-
-import * as S from './styles';
 import GameCard, { GameCardProps } from 'components/GameCard';
 import { HighlightProps } from 'components/Highlight';
 import Showcase from 'components/Showcase';
+import { Grid } from 'components/Grid';
+
+import * as S from './styles';
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[];
@@ -23,11 +25,13 @@ const Wishlist = ({
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
-    </Container>
 
-    {games?.map((game, index) => (
-      <GameCard key={index} {...game} />
-    ))}
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={index} {...game} />
+        ))}
+      </Grid>
+    </Container>
 
     <Showcase
       title="You may like these games"
